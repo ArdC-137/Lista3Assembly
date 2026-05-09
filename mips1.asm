ac9c5a3b1ae51b0d4588697217923939c4bc6f07
Pedro Arruda <85127477+ArdC-137@users.noreply.github.com>
2026-05-08 22:09:32 -0300

    Segunda e completa versão do código.

--- mips1.asm
+++ mips1.asm
@@ -35,4 +35,21 @@ main:
 	li $v0, 5
 	syscall
 	move $t2, $v0 		# $t2 = altura
-		
+	
+	# --- Calcula Volume = C * L * A ----
+	mul $t3, $t0, $t1 # $t3 = comp * larg
+	mul $t3, $t3, $t2 # $t3 = resultdo * altura
+	
+	# --- Exibe resultado ---
+	li $v0, 4
+	la $a0, msg_vol 
+	syscall
+	li $v0, 1		# syscall 1 = print_int
+	move $a0, $t3
+	syscall
+	li $v0, 4
+	la $a0, newlinw
+	syscall 
+	
+	i $v0, 10		# syscall 10 = exit
+	syscall 
